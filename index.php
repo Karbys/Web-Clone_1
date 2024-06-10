64c34dfd188a0ab628a57c5886a35aedb85ed369
Karbys <Bewantza33@gmail.com>
2024-06-11 00:45:27 +0700

    CloneEP_1

--- index.php
+++ index.php
@@ -3,9 +3,121 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Valorant</title>
+    <title>CloneEP_1</title>
+    <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    GG
+    <nav>
+        <div class="container">
+            <div class="nav-con">
+                <div class="logo">
+                    <a href="#">Bloom</a>
+                </div>
+                <ul class="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Sevices</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contract Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section class="hero">
+        <div class="container">
+            <div class="hero-con">
+                <div class="hero-info">
+                    <h3>This is hero</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus saepe sunt beatae, voluptatum at quia sapiente velit mollitia, ullam dolor inventore fugiat magnam adipisci, corporis eius molestias quibusdam voluptatibus sequi exercitationem. Dolorum nulla cum molestias. Minus voluptatibus accusamus, natus nemo esse sunt dignissimos labore ad adipisci incidunt reiciendis, architecto voluptas!</p>
+                    <a href="#" class="hero-btn">Learn more</a>        
+                </div>
+                <div class="hero-img">
+                    <img src="https://img.freepik.com/free-photo/big-sandwich-hamburger-with-juicy-beef-burger-cheese-tomato-red-onion-wooden-table_2829-19631.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="blog">
+        <div class="container">
+            <div class="blog-title">
+                <h3>Our Blog</h3>
+            </div>
+            <div class="blog-con">
+                <div class="blog-item">
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/026/794/680/small_2x/double-hamburger-isolated-on-white-background-fresh-burger-fast-food-with-beef-and-cream-cheese-realistic-image-ultra-hd-high-design-very-detailed-free-photo.jpg" alt="">
+                    <h4>My Blog</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti voluptate architecto explicabo ab distinctio animi eligendi quibusdam suscipit consequuntur sit.</p>
+                    <a href="#" class="blog-btn">Learn More</a>
+                </div>
+                <div class="blog-item">
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/026/794/680/small_2x/double-hamburger-isolated-on-white-background-fresh-burger-fast-food-with-beef-and-cream-cheese-realistic-image-ultra-hd-high-design-very-detailed-free-photo.jpg" alt="">
+                    <h4>My Blog</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti voluptate architecto explicabo ab distinctio animi eligendi quibusdam suscipit consequuntur sit.</p>
+                    <a href="#" class="blog-btn">Learn More</a>
+                </div>
+                <div class="blog-item">
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/026/794/680/small_2x/double-hamburger-isolated-on-white-background-fresh-burger-fast-food-with-beef-and-cream-cheese-realistic-image-ultra-hd-high-design-very-detailed-free-photo.jpg" alt="">
+                    <h4>My Blog</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti voluptate architecto explicabo ab distinctio animi eligendi quibusdam suscipit consequuntur sit.</p>
+                    <a href="#" class="blog-btn">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+   <div class="footer-top">
+        <div class="container">
+            <div class="footer-top-con">
+                <div class="footer-top-item">
+                    <h4>Lastest posts</h4>
+                    <div class="footer-top-item-con">
+                        <div class="date">
+                            JULY <br>
+                            27
+                        </div>
+                        <div class="info">
+                            <span class="info-title">Lorem ipsum dolor sit amet.</span>
+                            <span class="info-desc">Lorem ipsum dolor sit amet consectetur.</span>
+                        </div>
+                    </div>
+                    <div class="footer-top-item-con">
+                        <div class="date">
+                            JULY <br>
+                            27
+                        </div>
+                        <div class="info">
+                            <span class="info-title">Lorem ipsum dolor sit amet.</span>
+                            <span class="info-desc">Lorem ipsum dolor sit amet consectetur.</span>
+                        </div>
+                    </div>
+                    <div class="footer-top-item-con">
+                        <div class="date">
+                            JULY <br>
+                            27
+                        </div>
+                        <div class="info">
+                            <span class="info-title">Lorem ipsum dolor sit amet.</span>
+                            <span class="info-desc">Lorem ipsum dolor sit amet consectetur.</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-top-item">
+                    <h4>About</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quasi, aspernatur fuga doloremque illum voluptates similique. Rem illo inventore harum autem laboriosam ratione minus sint possimus dicta omnis odio, repellendus voluptas dolores recusandae facere fugit dolorum, soluta eos, distinctio natus atque! Mollitia sint minus tenetur unde qui rerum ullam iusto?</p>
+                    
+                </div>
+                <div class="footer-top-item">
+                    <h4>Stay Connected</h4>
+                    <p><i class="fa fa-facebook-official" aria-hidden="true"></i>Facebook</p>
+                    <p><i class="fa fa-twitter" aria-hidden="true"></i>Twitter</p>
+                    <p><i class="fa fa-rss-square" aria-hidden="true"></i>RSS</p>
+                    <p><i class="fa fa-google-plus-square" aria-hidden="true"></i>Google+</p>
+                </div>
+            </div>
+        </div>
+   </div>
+   <div class="footer-bottom">
+        <p>Copyright 2022. All right reserve Bloom web design.</p>
+   </div>
 </body>
 </html>
